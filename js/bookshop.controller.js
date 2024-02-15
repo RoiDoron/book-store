9ea51456e7b0b7f792f81af372c9cef654392833
Roi Doron <roidoron6@gmail.com>
2024-02-15 15:23:14 +0200

    Add renderBooks()

--- js/bookshop.controller.js
+++ js/bookshop.controller.js
@@ -1,8 +1,27 @@
 'use strict'
+
+
  function onInit(){
     renderBooks()
  }
 
  function renderBooks(){
-    
+    const books = getBooks()
+
+    const strHTML = '<tr><th>title</th><th>price</th><th>action</th></tr>'
+
+   const strHTMLcontinue = books.map(book=> 
+        `<tr>
+        <td>${book.title}</td>
+        <td>${book.price}</td>
+        <td>
+            <button class="read">Read</button>
+            <button class="update">Update</button>
+            <button class="delete">Delete</button>
+        </td>
+    </tr>`)
+        
+    console.log(strHTMLcontinue.join(''))
+        const elBooksTable =document.querySelector('.books-table')
+        elBooksTable.innerHTML =  strHTML+strHTMLcontinue.join('')
  }
